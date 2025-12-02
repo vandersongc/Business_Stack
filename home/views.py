6c62a8514b8baff99e4f5f44a8ea99b6161a9b97
vandersongc <vandersongc@gmail.com>
2025-12-02 20:57:39 -0300

    v2 2012

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Crie a função 'home' que o seu urls.py está procurando
 def home(request):
-    return render(request, 'home/index.html') # Ou HttpResponse("Olá Mundo") se ainda não tiver templates
+    return render(request, 'home.html') 
