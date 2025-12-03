4d13184db8d5885db4af07126336a2c772fabc26
vandersongc <vandersongc@gmail.com>
2025-12-03 15:56:06 -0300

    v1 0312

--- home/urls.py
+++ home/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
+from home import views
+
+
+urlpatterns = [
 path('', home_views.home, name='home'),
+path('cadastro/', views.cadastro, name='cadastro'),
+path('login/', views.login_view, name='login'),
+
+]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Crie a função 'home' que o seu urls.py está procurando
 def home(request):
     return render(request, 'home.html') 
+
+def cadastro(request):
+    return render(request, 'cadastro.html')
+
+def login(request):
+    return render(request, 'login.html')
