addceb3fcf8d9633bd25074371058ff490d090fe
vandersongc <vandersongc@gmail.com>
2025-12-05 19:16:45 -0300

    v2 0512

--- business_stack/urls.py
+++ business_stack/urls.py
@@ -1,32 +1,20 @@
-"""
-URL configuration for business_stack project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# business_stack/urls.py
 from django.contrib import admin
 from django.urls import path
-from home import views as home_views  # Importando da app home
-from . import views as temp_views     # Importando as views temporárias (se ainda as usar)
+from home import views as home_views
+from . import views as temp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # Rota da Página Inicial (Apontando para a app home)
+    # Rota da Página Inicial
     path('', home_views.home, name='home'),
 
-    # Rotas dos Módulos (Se ainda não tiver views específicas nas outras apps, pode manter as temporárias)
+    # --- ROTAS DE AUTENTICAÇÃO (Adicione isto) ---
+    path('cadastro/', home_views.cadastro, name='cadastro'),
+    path('login/', home_views.login, name='login'),
+    # ---------------------------------------------
+
+    # Rotas dos Módulos (Views temporárias)
     path('administrador/', temp_views.administrador, name='administrador'),
     path('auditoria/', temp_views.auditoria, name='auditoria'),
     path('contabilidade/', temp_views.contabilidade, name='contabilidade'),
